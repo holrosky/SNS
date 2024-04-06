b934aecb76cab20d8076e953b46035158afe26ed
Taehee <holrosky5@gmail.com>
2024-04-06 13:15:55 +0900

    custom theme applied

--- app/src/main/java/com/kthkr/sns/core/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/kthkr/sns/core/presentation/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val BlueLight = Color(0xFFD7E3FC) // 밝은 파란색, 주로 배경에 사용
+val BlueGreyLight = Color(0xFFB0BEC5) // 밝은 청회색, 보조 텍스트나 아이콘에 사용
+val TealLight = Color(0xFF80CBC4) // 밝은 청록색, 강조 요소나 액션 버튼에 사용
+
+// 어두운 테마 색상
+val BlueDark = Color(0xFF455A64) // 어두운 파란색, 주로 배경에 사용
+val BlueGreyDark = Color(0xFF37474F) // 어두운 청회색, 보조 텍스트나 아이콘에 사용
+val TealDark = Color(0xFF00695C) // 어두운 청록색, 강조 요소나 액션 버튼에 사용
--- app/src/main/java/com/kthkr/sns/core/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/kthkr/sns/core/presentation/ui/theme/Type.kt
@@ -2,33 +2,37 @@ package com.kthkr.sns.core.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kthkr.sns.R
 
+val jalnaGothic = FontFamily(
+    Font(R.font.jalnan_gothic)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = jalnaGothic,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-titleLarge = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
-    fontSize = 22.sp,
-    lineHeight = 28.sp,
-    letterSpacing = 0.sp
-),
-labelSmall = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Medium,
-    fontSize = 11.sp,
-    lineHeight = 16.sp,
-    letterSpacing = 0.5.sp
-)
-*/
+
 )
